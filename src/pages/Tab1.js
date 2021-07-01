61cce25fa8f861d1eaaff7981f30a5a810c2c200
LAPTOP-U5H1VM0L\cibel <cibelle@cyberdive.co>
2021-07-01 16:43:48 -0700

    map over contacts list

--- src/pages/Tab1.js
+++ src/pages/Tab1.js
@@ -1,20 +1,28 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import {
 	IonContent,
-	IonHeader,
+	IonAvatar,
 	IonPage,
 	IonTitle,
+	IonItem,
+	IonLabel,
+	IonList,
 	IonToolbar,
-	IonButton
+	IonButton,
+	IonText
 } from "@ionic/react";
 import ExploreContainer from "../components/ExploreContainer";
 import "./Tab1.css";
 
 import { AppContext } from "../State.js";
 
+import Utility from "../Utility";
+
 const Tab1 = () => {
 	//Defines which context we want to use int this component
 	const { state, dispatch } = useContext(AppContext);
+	let contacts = state.user.contacts
+
 
 
 	const changeName = () => {
@@ -25,14 +33,43 @@ const Tab1 = () => {
 			}
 		})
 	}
+	useEffect(() => {
+		console.log(contacts)
+		//  return () => {
+		// 	 cleanup
+		//  }
+	}, [])
+
 
 	return (
 		<IonPage>
 
-			<IonContent fullscreen>
+			<IonContent className="chat-screen">
 
-				<ExploreContainer name="Tab 1 page" />
-			
+				<IonList>
+
+					{
+						contacts.map((item) =>
+							<IonItem
+								key={item.user_id} >
+								<IonAvatar >
+									<img
+										src={item.photo} />
+								</IonAvatar>
+								<IonLabel>
+									<h3>{item.name}</h3>
+									<p>Hello, did you get to talk with them about..</p>
+								</IonLabel>
+								{/* <IonLabel>
+									<p>{Utility.getToday(item.last_seen)}</p>
+								</IonLabel> */}
+							</IonItem>
+						)}
+				</IonList>
+
+
+
+
 			</IonContent>
 		</IonPage>
 	);
